5ffc8a13f6ba71a3b7100048a64dd8883ea9e531
João Henrique M. de Lucena <112053980+jaohenriqu3@users.noreply.github.com>
2023-11-01 00:39:15 -0300

    Create Table_Voluntarios.sql

---
+++ Script MySQL/bdacsocial/Table_Voluntarios.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Voluntarios (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  `Nome` varchar(50) DEFAULT NULL,
+  `Nacionalidade` varchar(50) DEFAULT NULL,
+  `EstadoCivil` varchar(20) DEFAULT NULL,
+  `Profissao` varchar(30) DEFAULT NULL,
+  `RG` int DEFAULT NULL,
+  `CPF` int DEFAULT NULL,
+  `Endereco` varchar(50) DEFAULT NULL,
+  `Numero` int DEFAULT NULL,
+  `Complemento` varchar(20) DEFAULT NULL,
+  `Bairro` varchar(20) DEFAULT NULL,
+  `Codigo_postal` int DEFAULT NULL,
+  `Cidade` varchar(30) DEFAULT NULL,
+  `Estado` varchar(20) DEFAULT NULL ) 
